4e6c43a5569cd8383441c966d29618b97fb47651
fwy-olives <417404058@qq.com>
2021-01-19 11:41:03 +0800

    配置url处理；

--- src/Lyfzcom/Config.php
+++ src/Lyfzcom/Config.php
@@ -10,7 +10,7 @@ final class Config
 
     const DEV_CENTER  = 'https://api.27yn.cn/companyCenter/v1/';              // 企业中心测试
 
-    const API_LOGIN  = 'https://api.27yn.cn/sso/v1/';              // 登录系统
+    const API_LOGIN  = 'https://api.lyfz.net/sso/v1/';              // 登录系统
 
-    const DEV_LOGIN  = 'https://api.lyfz.net/sso/v1/';              // 登录系统 测试
+    const DEV_LOGIN  = 'https://api.27yn.cn/sso/v1/';              // 登录系统 测试
 }
